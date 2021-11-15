4d4754cc077830292af0faccd2b69fc1a4a37f69
hossein <hossein.abdi7@gmail.com>
2021-11-15 19:27:56 +0330

    read data From Text file and create a List Of Product object acording each line of TxtFile

---
+++ SellsManager/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    public class Product
+    {
+        public int RecordNumber { get; set; }
+        public int ProductId { get; set; }
+        public int Price { get; set; }
+        public int Quantity { get; set; }
+        public override string ToString()
+        {
+            return $"{RecordNumber}|{ProductId}|{Price}|{Quantity}";
+        }
+
+    }
+}
---
+++ SellsManager/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Task1;
+using System.Linq;
+
+string path = @"D:\saleList.txt";
+string[] txtFile = File.ReadAllLines(path);
+List<Product> files = new List<Product>();
+foreach (var item in txtFile)
+{
+    Product SampleProduct = new Product();
+    string[] data=item.Split('|');
+    if (data.Length > 0)
+    { 
+      SampleProduct .RecordNumber= Convert.ToInt32(data[0]);
+       SampleProduct.ProductId= Convert.ToInt32(data[1]);
+         SampleProduct.Price= Convert.ToInt32(data[2]);
+          SampleProduct.Quantity= Convert.ToInt32(data[3]);
+    }
+    files.Add(SampleProduct);
+}
+
+var query = files.GroupBy(x => x.ProductId).Select(x=> x.Sum(y=>y.Quantity)).Max(); 
+      
+            
+
+    //foreach(var item in query)
+    Console.WriteLine(query);
